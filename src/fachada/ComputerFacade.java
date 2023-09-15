e7684fc2d4177bf7452c5aaa0980f26152bbf2b0
luig04 <luischilito@itipopayan.edu.co>
2023-09-15 00:37:35 -0500

    Computer

---
+++ src/fachada/ComputerFacade.java
@@ -0,0 +1,22 @@
+package fachada;
+
+public class ComputerFacade {
+    private CPU cpu;
+    private HardDrive hardDrive;
+    private Memory memory;
+
+    public ComputerFacade(){
+        this.cpu = new CPU();
+        this.hardDrive = new HardDrive();
+        this.memory = new Memory();
+    }
+
+        public void Start(){
+            System.out.println("STARTING...");
+            cpu.freeze();
+            memory.load();
+            hardDrive.read();
+            cpu.jump();
+            cpu.execute();
+    }
+}
